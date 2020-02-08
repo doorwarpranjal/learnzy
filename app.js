91579483387cc203c620e3b5386344733d21e94c
pranjal doorwar <doorwarpranjal@gmail.com>
2020-02-09 03:34:24 +0530

    first push

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const port  = 8000 ;
+const port  = process.env.PORT||8000 ;
 const express = require('express');
 const mongodb = require('mongodb');
 const db = require('./config/connection.js') ;
